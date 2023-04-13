d56eeb3a4286f79a5f19dec500e93161ae111689
Quang Minh <vudinhquangminh1909@gmail.com>
2023-04-13 09:27:03 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let snow = document.getElementById('snow');
+let count = 80;
+for (var i = 0; i < 80; i++) {
+    let div = document.createElement('div');
+    let leftSnow = Math.floor(Math.random() * snow.clientWidth);
+    let topSnow = Math.floor(Math.random() * snow.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5)) + 5;
+    let blurSnow = Math.floor(Math.random() * 6);
+    div.classList.add('snow-icon');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    snow.appendChild(div);
+}
